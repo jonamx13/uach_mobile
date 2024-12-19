61acbeb4962547cc72b5f36610d4be118dc8791f
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2024-12-19 03:01:39 -0600

    Only directories section are scrollable

--- lib/features/directorio/directorio_screen.dart
+++ lib/features/directorio/directorio_screen.dart
@@ -7,53 +7,59 @@ class DirectorioScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 52.0, bottom: 16.0),
-                child: Center(
-                  child: Text(
-                    'Directorio',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                      height: 20 / 16,
-                      color: Color(0xFF0E1016),
-                    ),
+        child: Column(
+          children: [
+            // LandImage (No será scrolleable)
+            Padding(
+              padding: const EdgeInsets.only(top: 52.0, bottom: 16.0),
+              child: Center(
+                child: Text(
+                  'Directorio',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    height: 20 / 16,
+                    color: Color(0xFF0E1016),
                   ),
                 ),
               ),
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 5,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/rectoria_photo.png'),
-                    fit: BoxFit.cover,
+            ),
+            // Land photo (No será scrolleable)
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 5,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/rectoria_photo.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            // Aquí mapeamos los datos y pasamos cada grupo al DirectorioCard
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: directorioData.map((directorioGrupo) {
+                      return Column(
+                        children: [
+                          DirectorioCard(
+                            titulo: directorioGrupo['directorio'],
+                            contactos: List<Map<String, String>>.from(
+                                directorioGrupo['lista_contactos']),
+                          ),
+                          SizedBox(height: 20), // Añadimos un espaciado entre los grupos
+                        ],
+                      );
+                    }).toList(),
                   ),
                 ),
               ),
-              // Aquí mapeamos los datos y pasamos cada grupo al DirectorioCard
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: directorioData.map((directorioGrupo) {
-                    return Column(
-                      children: [
-                        DirectorioCard(
-                          titulo: directorioGrupo['directorio'], 
-                          contactos: List<Map<String, String>>.from(directorioGrupo['lista_contactos']),
-                        ),
-                      ],
-                    );
-                  }).toList(),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
